40a228c8f32d167261d3169797f9b3df8c8c9a44
Terrell <wrightterrell049@gmail.com>
2022-09-17 14:19:55 -0400

    styles continued >.>

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,32 @@
 import './styles/index.css'
+import React, { useState } from 'react';
 
 export default function App() {
-  const userlogedin = false;
+  const [userlogedin, setlogin] = useState(false)
+
   return (
   <div>
-    <div>
+    <div className='userlogin'>
       {!userlogedin ? (
-        <form>
-        <input type="text" name="text" class="input" placeholder="Type something here...."></input>
-      </form>
+          <div className="loginform">
+            <label>USERNAME</label>
+            <input type="text" name="user" className="input" placeholder="Type something here...."></input>
+            <label>USERNAME</label>
+            <input type="text" name="user" className="input" placeholder="Type something here...."></input>
+            <div className="dropdown">
+              <selectmenu name="room" id="topic" className="joinroom">
+                <option value="javascript">Javascript</option>
+                <option value="react">react</option>
+                <option value="node">node</option>
+                <option value="databases">databases</option>
+              </selectmenu>
+
+            </div>
+
+            <button onClick={()=>{setlogin(true)}}>Enter</button>
+          </div>
+          
+      
       ):(
         <div>
           chat room
